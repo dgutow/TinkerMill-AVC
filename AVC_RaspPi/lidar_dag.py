0e5597e9155100b61ba087979da031d38f760d69
unknown <djgutow@gmail.com>
2018-08-12 22:29:37 +0100

    lidar_dag

---
+++ AVC_RaspPi/lidar_dag.py
@@ -0,0 +1,79 @@
+import serial
+from rplidar import RPLidar
+import time
+
+
+#PORT_NAME = '/dev/ttyUSB0'
+
+
+###############################################################################
+# scan 
+############################################################################### 
+def scan():
+    lidar = RPLidar(portname)
+    
+    while (True): 
+        
+        time.sleep (0.1)  
+        
+        curr_time = time.time()
+        print ( "MAIN_LOOP - curr_time %f\n" % (curr_time) )
+        
+    
+    
+    
+    
+    
+    
+    
+###############################################################################
+# stopScan 
+###############################################################################    
+def stopScan():
+    lidar = RPLidar(portname) 
+    lidar.stop()
+    lidar.stop_motor()
+    lidar.disconnect()   
+# end stopScan    
+
+###############################################################################
+# MAIN-LOOP EXECUTION
+###############################################################################
+if __name__ == "__main__":
+    ##### TEST # 1 
+    #initializations()
+    scan()    
+# end    
+
+"""
+class LIDAR(object):
+    def __init__(self, portname):
+        self.portname = portname
+
+    def scan(self):
+        pointCloud = []
+        lidar = RPLidar(self.portname)
+
+        for i, measurement in enumerate(lidar.iter_measures()):
+            for j, v in enumerate(measurement):
+                if j == 0:
+                    newscan = v
+                if j == 1:
+                    quality = v
+                if j == 2:
+                    angle = v
+                if j == 3:
+                    length = v
+
+            # change angle to match orientation of vehicle
+            angle = -1 * (angle - 90)
+
+            pointCloud.append([angle, length])
+
+            if i > 360:
+               break
+
+
+
+        return pointCloud
+"""
