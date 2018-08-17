d921baa36eee87a0531b52b1f02e17c2b4b1d940
dgutow <djgutow@gmail.com>
2018-08-16 19:29:47 -0600

    lidar work

--- AVC_RaspPi/lidar_dag.py
+++ AVC_RaspPi/lidar_dag.py
@@ -1,5 +1,5 @@
 import serial
-from rplidar import RPLidar
+from   rplidar import RPLidar
 import time
 
 
@@ -8,27 +8,13 @@ PORT_NAME = '/dev/ttyUSB0'
 lidar    = None
 scandata = None
 scan_iter= None
+
 ###############################################################################
-# init_scan0
+# init_scan 
 ############################################################################### 
-'''def init_scan0():
-    global scandata
+def init_scan():
     global lidar
-    
-    lidar = RPLidar(PORT_NAME )
-    #info = lidar.get_info()
-    #print (info)
-    
-    lidar.start_motor()
-    scandata = lidar.iter_scans()
-#end init_scan    
-'''
-###############################################################################
-# init_scan1 
-############################################################################### 
-def init_scan1():
     global scandata
-    global lidar
     global scan_iter
     
     lidar = RPLidar(PORT_NAME )
@@ -61,62 +47,76 @@ def init_scan1():
 ###############################################################################
 # scan1 
 ############################################################################### 
-def scan1():
+def get_scan():
     global scan_iter
     
-    while (True): 
-        
-        time.sleep (0.2)  
-        
-        curr_time = time.time()
-        print ( "------------- PRE-TIME %f" % (curr_time) )
-        
-        new_cnt = 0
-        old_cnt = 0                              
-        scan_list = []
-        iterator = lidar.iter_measures('normal', 3000)
-        for new_scan, quality, angle, distance in iterator:
-            if new_scan:
-                new_cnt += 1
-                if len(scan_list) > 5:
-                    break
-            else:
-                old_cnt += 1                                    
-            if distance > 0:
-                scan_list.append((quality, angle, distance))                
-                
-                
-                
-                
-                
-        print ( "new_cnt %d old_cnt %d" % (new_cnt, old_cnt) )
-        print ( "measure time %f\n" % ( time.time() - curr_time ) )
-    # end while
+    new_cnt     = 0
+    old_cnt     = 0  
+    zero_cnt    = 0    
+    scan_list   = []
+    
+    iterator = lidar.iter_measures('normal', 10000)
+    for new_scan, quality, angle, distance in iterator:
+        if distance > 0:
+            scan_list.append((new_scan, quality, angle, distance))  
+        else:
+            zero_cnt += 1
+    
+        if new_scan:
+            new_cnt += 1
+            if len(scan_list) > 5:
+                break
+        else:
+            old_cnt += 1   
+
+    # end for
             
+    print ( "new_cnt %d old_cnt %d zero_cnt %d" % (new_cnt, old_cnt, zero_cnt) )
+
+    return scan_list        
+    
 #end scan()        
     
 ###############################################################################
-# stopScan 
+# stop_scan 
 ###############################################################################   
-def stopScan():
+def stop_scan():
     if lidar != None:
         lidar.stop()
         lidar.stop_motor()
         lidar.disconnect()   
-# end stopScan    
+# end stop_scan    
 
 ###############################################################################
 # MAIN-LOOP TESTING
 ###############################################################################
 if __name__ == "__main__":
-    init_scan1()
+    init_scan()
+
     try:
-        scan1() 
+        while (True):      
+            time.sleep (0.2)  
+            
+            curr_time = time.time()
+            print ( "------------- PRE-TIME %f" % (curr_time) )    
+            scan_list = get_scan()
+            print ( "measure time %f\n" % ( time.time() - curr_time ) )
+            
+            nScans = len(scan_list)
+            for dataPt in scan_list:
+               newPt = dataPt[0]
+               qual  = dataPt[1]
+               angle = dataPt[2]
+               dist  = dataPt[3]
+               print ("New %d, qual %d, angle %d, dist %d) % (newPt, qual, angle, dist))
+            # end for
+            
+        # end while        
+  
     except:
         pass
         
-    stopScan()    
-        
+    stop_scan()       
 # end    
 
 
