fd07bda4ee38ca60821bb7bb8aecc22ac8ade5bf
fmccamer <fmccamer@gmail.com>
2019-01-06 16:58:39 -0700

    further changes to wallDet

--- AVC_RaspPi/wallDet.py
+++ AVC_RaspPi/wallDet.py
@@ -42,33 +42,74 @@ def plotBuffer(lidarBuffer):
     plt.grid(True)
 
     #plt.show(block=False)
-    plt.show()
+    #plt.show()
     #plt.draw()
-    plt.pause(0.001)
+    #plt.pause(0.001)
 
 def processBuffer(vehState):
+    VEHICLE_WIDTH = 1.5*ct.METERS_PER_FOOT
+
     plotBuffer(vehState.lidarBuffer)
     
-    # okay our goal here is primarily to test if we can do quadratic fitting of points
+    # okay our goal here is primarily to test wall detection
     
     # first things first lets convert the lidarbuffer points into cartesian coordinates
-    #angles= np.expand_dims(np.linspace(0,2*math.pi,360),1)
-    #distances = np.expand_dims(vehState.lidarBuffer[:,ct.LIDAR_BUFFER_DISTANCE],1)
-    #points = np.concatenate((np.sin(angles)*distances, \
-    #                         np.cos(angles)*distances \
-    #                         ),axis=1)
+    allAngles= np.expand_dims(np.linspace(0,2*math.pi,360),1)
+    allDistances = np.expand_dims(vehState.lidarBuffer[:,ct.LIDAR_BUFFER_DISTANCE],1)
+    points = np.concatenate((np.sin(allAngles)*allDistances, \
+                             np.cos(allAngles)*allDistances \
+                             ),axis=1)
     
-    # now let's try a toy problem, create a quadratic fit to the first 5 points
-    # set the mean angle to 90 deg so that we get an optimally 
-    
+    # now let's try linking all of the points that are within the vehicle width
+    groups = np.zeros(allDistances.shape)
+    for i in range(points.shape[0]):
+        if not (groups[i] == 0) or (allDistances[i]==12):
+            continue
+        # this is an unallocated point of a distance shorter than 12
+
+        # allocate this point
+        groups[i]=np.max(groups)+1
+        # find the unallocate points
+        indices = np.nonzero((groups==0) * (allDistances < 12))[0]
+        # calculate the distances from this point
+        distances = np.sqrt(np.sum(np.square(points[indices,:]-points[i,:]),axis=1))
+        # get a list of all unallocated points near this one
+        toBeAdded = indices[np.where(distances<=VEHICLE_WIDTH)].tolist()
+        # allocate them to prevent looping
+        groups[toBeAdded]=groups[i]
+        while len(toBeAdded):
+            curIndex = toBeAdded.pop()
+            # find the unallocate points
+            indices = np.nonzero((groups==0) * (allDistances < 12))[0]
+            # calculate the distances from this point
+            distances = np.sqrt(np.sum(np.square(points[indices,:]-points[curIndex,:]),axis=1))
+            # get a list of all unallocated points near this one
+            newAdditions = indices[np.where(distances<=VEHICLE_WIDTH)].tolist()
+            # allocate them to prevent looping
+            groups[newAdditions]=groups[i]
+            # add these elements to the existing list
+            toBeAdded.extend(newAdditions)
+            
+    # now display the results
+    for i in range(1,np.max(groups).astype(int).item()+1):
+        indices = np.nonzero(groups==i)[0]
+        if len(indices)>5:
+            plt.plot(points[indices,0],points[indices,1],linestyle=' ',marker='o',markersize=10,color='g')
+    plt.show()
+    #plt.draw()
+    plt.pause(0.001)        
+    groups
+        
+        
 ###############################################################################
 # Test code
 ###############################################################################
-TEST = 2
+TEST = 1
 NPY_DIR = "."
 MAT_DIR = "./matlabCourse/"
 if __name__ == '__main__':
     import sys
+    plt.ion()
     #print(sys.paRth)
 
     ##########################################################################
@@ -122,7 +163,7 @@ if __name__ == '__main__':
             
             processBuffer(vehState)
             #plotBuffer(vehState.lidarBuffer)
-            plt.pause(1)
+            plt.pause(.1)
     #vehState.lidarBuffer = np.load(dir[0])
 
 # end
